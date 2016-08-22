365731b2ccd4591a48652407165628dbe0c2210e
James Wyatt Cready <jcready@gmail.com>
2016-08-22 10:19:20 -0400

    More testing

--- test/send.js
+++ test/send.js
@@ -5,13 +5,17 @@ var fs = require('fs')
 describe('Attempting to close a bad file descriptor', function () {
   it('should output a EBADF error code', function (done) {
     var fd = fs.openSync('./package.json', 'r')
-    fs.createReadStream(null, {
+    var stream = fs.createReadStream(null, {
       fd: fd,
       end: 0
     }).on('end', function () {
       fs.close(fd, function (e) {
         if (e) {
           console.log('fs.close', e)
+          setTimeout(function () {
+            console.log('manually triggering "error" event on stream')
+            stream.emit('error', e)
+          }, 1000)
         }
       })
     }).on('error', function (e) {
